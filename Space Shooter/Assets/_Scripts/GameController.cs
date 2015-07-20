f7cbf90b757acf8d71c1679b50590e943331b99e
Rahat Mahbub <rahatm1@uvic.ca>
2015-07-19 20:20:16 -0700

    Lesson 2: Space Shooter Complete

---
+++ Space Shooter/Assets/_Scripts/GameController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public Text scoreText;
+	private int score;
+
+	public Text gameOverText;
+	public Text restartText;
+
+	public GameObject restartBtnObject;
+	public GameObject canvasObj;
+
+	private bool gameOver;
+	private bool restart;
+
+	void Start()
+	{
+		gameOverText.text = "";
+		restartText.text = "";
+		gameOver = false;
+		restart = false;
+
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				Restart();
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		
+		while (true) {
+			for (int i=0; i<hazardCount;i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' to restart game!";
+				restart = true;
+
+				GameObject restartBtn = Instantiate(restartBtnObject) as GameObject;
+				restartBtn.transform.SetParent(canvasObj.transform, false);
+
+				Button rbt = restartBtn.GetComponent<Button>();
+				rbt.onClick.AddListener(() => Restart());
+
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScore)
+	{
+		score += newScore;
+		UpdateScore ();
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+
+	public void Restart()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
---
+++ Space Shooter/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+
+		float moveVertical = Input.acceleration.y;
+		float moveHorizontal = Input.acceleration.x;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
